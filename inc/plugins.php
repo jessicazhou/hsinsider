be8f01d9d2a492b9514274a38911f1125a6a8b2d
Perez, James <James.Perez3@latimes.com>
2016-01-14 11:38:08 -0800

    removed zoninator

--- inc/plugins.php
+++ inc/plugins.php
@@ -5,7 +5,6 @@
 if ( function_exists( 'wpcom_vip_load_plugin' ) ) {
 	wpcom_vip_load_plugin( 'fieldmanager' );
 	wpcom_vip_load_plugin( 'co-authors-plus' );
-	wpcom_vip_load_plugin( 'zoninator' );
 	wpcom_vip_load_plugin( 'chartbeat' );
 	wpcom_vip_load_plugin( 'storify' );
 	wpcom_vip_load_plugin( 'edit-flow' );
