104c9c26dedecae2cbc98c00b7c6b93f1a1a8ce9
Perez, James <James.Perez3@latimes.com>
2016-03-14 16:21:39 -0700

    Launch tweaks

---
+++ client/js/global.js
@@ -0,0 +1,2 @@
+// Hide lazy load gravatar images
+jQuery('img[src="https://s0.wp.com/wp-content/themes/vip/plugins/lazy-load/images/1x1.trans.gif"]').hide();
--- static/js/global.js
+++ static/js/global.js
@@ -2350,6 +2350,8 @@ jQuery( document ).ready( function( $ ) {
 	}
 } );
 
+// Hide lazy load gravatar images
+jQuery('img[src="https://s0.wp.com/wp-content/themes/vip/plugins/lazy-load/images/1x1.trans.gif"]').hide();
 
 /**
  * LA Times HS Insider Maps
