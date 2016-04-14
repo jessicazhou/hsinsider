8c524d1205def3297942a8c0f6362ea38c1f6261
Perez, James <James.Perez3@latimes.com>
2016-04-14 12:39:20 -0700

    Navbar fix, dropcap, better titles and widget tweak

--- client/js/global.js
+++ client/js/global.js
@@ -2,6 +2,5 @@
 jQuery('img[src="https://s0.wp.com/wp-content/themes/vip/plugins/lazy-load/images/1x1.trans.gif"]').hide();
 
 if( jQuery( '.BrightcoveExperience' ).length != -1 ) {
-  alert('do the thing');
   brightcove.createExperiences();
 }
